3291548f95a60f1f2800f207e8f08489c0ad64d2
Samuel Dagostino <dagostinosamuel@outlook.com>
2026-03-04 15:58:17 -0500

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Mare from './mare-site.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Mare />
+  </React.StrictMode>
+)
